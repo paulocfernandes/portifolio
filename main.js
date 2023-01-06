42b5455dcef496a16c0ff03ad08b9e0c92538c15
paulocfernandes <pauloregisc25@gmail.com>
2023-01-06 15:43:23 -0300

    iniciado javaScripit

---
+++ main.js
@@ -0,0 +1,26 @@
+const butao = document.getElementById("switch")
+butao.addEventListener("click", toggleMode )
+
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains('light')) {
+
+    img.setAttribute("src", "./assed/Ellipse.png")
+  } else {
+    img.setAttribute("src", "./assed/Avatar.png")
+  }
+}
+
+
+
+
+
+
+/*function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+}*/
